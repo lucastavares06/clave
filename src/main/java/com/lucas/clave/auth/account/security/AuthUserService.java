2b8362b8e94070649980531c218489c7ff16eaf5
lucastavares06 <lucastavareswin10@outlook.com>
2025-03-29 00:01:30 -0300

    refactor: use user service instead of repository

--- src/main/java/com/lucas/clave/auth/account/security/AuthUserService.java
+++ src/main/java/com/lucas/clave/auth/account/security/AuthUserService.java
@@ -1,7 +1,9 @@
 package com.lucas.clave.auth.account.security;
 
 import com.lucas.clave.auth.user.UserRepository;
+import com.lucas.clave.auth.user.UserService;
 import com.lucas.clave.auth.user.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,17 +13,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AuthUserService implements UserDetailsService {
 
-    private final UserRepository userRepository;
-
-    public AuthUserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        User user = userService.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
 
         return AuthUser.builder()
