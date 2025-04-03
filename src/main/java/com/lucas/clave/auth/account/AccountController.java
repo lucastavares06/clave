06fffc2cc4d79c666dd9f445a269723793cd9a12
lucastavares06 <lucastavareswin10@outlook.com>
2025-04-02 21:50:08 -0300

    refactor: apply required args constructor to account controller

--- src/main/java/com/lucas/clave/auth/account/AccountController.java
+++ src/main/java/com/lucas/clave/auth/account/AccountController.java
@@ -2,18 +2,16 @@ package com.lucas.clave.auth.account;
 
 import com.lucas.clave.auth.account.model.*;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
-
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     @PostMapping("/login")
     public LoginResponse login(@Valid @RequestBody LoginRequest request) {
